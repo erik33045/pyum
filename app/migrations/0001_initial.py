df46969a04ef455e649d22bc28a9fbd7a62e13ea
erik <erik33045@gmail.com>
2014-03-18 11:37:19 -0400

    - Added styling to the login page. - Added some redirect links so people can actually navigate the site

---
+++ app/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from south.db import db
+from south.v2 import SchemaMigration
+
+
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding model 'User'
+        db.create_table(u'app_user', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal(u'app', ['User'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'User'
+        db.delete_table(u'app_user')
+
+
+    models = {
+        u'app.user': {
+            'Meta': {'object_name': 'User'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['app']
