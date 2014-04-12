239557ae9e92085d9eddfec7405d916c6bc6933f
erik <erik33045@gmail.com>
2014-04-12 14:06:29 -0400

    Added validation to text on search recipes page

--- app/views.py
+++ app/views.py
@@ -12,101 +12,48 @@ import YummlyDriver
 
 
 def register(request):
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
     registered = False
 
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
         profile_form = AppUserForm(data=request.POST)
 
-        # If the two forms are valid...
         if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data to the database.
             user = user_form.save()
-
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
             user.set_password(user.password)
             user.save()
 
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
             profile = profile_form.save(commit=False)
             profile.user = user
-
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            # Now we save the UserProfile model instance.
             profile.save()
-
-            # Update our variable to tell the template registration was successful.
             registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
         else:
             print user_form.errors, profile_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
     else:
         user_form = UserForm()
         profile_form = AppUserForm()
 
-    # Render the template depending on the context.
-    # noinspection PyUnresolvedReferences
     return render(request, 'app/register.html',
                   {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 
 def user_login(request):
-    # Like before, obtain the context for the user's request.
-    context = RequestContext(request)
-
-    # If the request is a HTTP POST, try to pull out the relevant information.
     if request.method == 'POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
         username = request.POST['username']
         password = request.POST['password']
 
-        # Use Django's machinery to attempt to see if the username/password
-        # combination is valid - a User object is returned if it is.
         user = authenticate(username=username, password=password)
 
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching credentials was found.
         if user is not None:
-            # Is the account active? It could have been disabled.
             if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage.
                 login(request, user)
                 return render(request, 'app/home.html', {})
             else:
-                # An inactive account was used - no logging in!
                 return HttpResponse("Your Pyum account is disabled.")
         else:
-            # Bad login details were provided. So we can't log the user in.
             print "Invalid login details: {0}".format(username)
             return HttpResponse("Invalid login details supplied.")
-
-    # The request is not a HTTP POST, so display the login form.
-    # This scenario would most likely be a HTTP GET.
     else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object...
-        # noinspection PyUnresolvedReferences
         return render(request, 'app/login.html', {})
 
 
@@ -118,7 +65,7 @@ def search_recipes(request):
             # query for the matches
             results = YummlyDriver.search_recipes(parameters).matches
             #map to table data and store in session
-            request.session['results'] = YummlyDriver.search_recipes(parameters).matches
+            request.session['results'] = results
 
         if not request.session.has_key('results'):
             return render(request, 'recipes.html', {'table': app.tables.ResultTable([])})
@@ -136,29 +83,12 @@ def search_recipes(request):
 def home(request):
     if request.user.is_authenticated():
         if request.method == "POST":
-            return httpResponsRedirect('recipes.htm')
+            return httpResponsRedirect('recipes.html')
 
         return render(request, 'app/home.html', {})
     else:
         return render(request, 'app/login.html', {})
 
-
 def user_logout(request):
     logout(request)
     return render(request, 'app/login.html', {})
-
-
-def profile(request):
-    if request.method == 'POST': # If the form has been submitted...
-        # ContactForm was defined in the the previous section
-        form = ProfileForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
-    else:
-        form = ProfileForm() # An unbound form
-
-    return render(request, 'profile.html', {
-        'form': form,
-    })
